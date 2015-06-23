9bd4c35060dc3ca30350b11daa1d755e2adbe042
karlosn <karlosn@gmaill.com>
2015-06-22 20:37:38 -0430

    Master

---
+++ IProveedorServicio.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ProveedorServicio
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IProveedorServicio
+    {
+        #region Proveedor
+        [OperationContract]
+        bool InsertarProveedor(Modelo.Proveedor _proveedor);
+
+        [OperationContract]
+        bool ActualizarProveedor(Modelo.Proveedor _proveedor);
+        
+        [OperationContract]
+        bool EliminarProveedor(Modelo.Proveedor _proveedor);
+        
+        [OperationContract]
+        List<Modelo.Proveedor> GetProveedores();
+        
+        [OperationContract]
+        Modelo.Proveedor GetProveedor(int _idProveedor);
+        #endregion
+
+        #region Categoria
+        [OperationContract]
+        bool InsertarCategoria(Modelo.Categoria _categoria);
+        
+        [OperationContract]
+        bool ActualizarCategoria(Modelo.Categoria _categoria);
+        
+        [OperationContract]
+        bool EliminarCategoria(Modelo.Categoria _categoria);
+        
+        [OperationContract]
+        List<Modelo.Categoria> GetCategorias();
+        
+        [OperationContract]
+        Modelo.Categoria GetCategoria(int _idCategoria);
+        #endregion
+
+        #region Producto
+        [OperationContract]
+        bool InsertarProducto(Modelo.Producto _producto);
+        
+        [OperationContract]
+        bool ActualizarProducto(Modelo.Producto _producto);
+        
+        [OperationContract]
+        bool EliminarProducto(Modelo.Producto _producto);
+        
+        [OperationContract]
+        List<Modelo.Producto> GetProductos();
+        
+        [OperationContract]
+        Modelo.Producto GetProducto(int _idProducto);
+        #endregion
+
+        //[OperationContract]
+        //string GetData(int value);
+
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    //[DataContract]
+    //public class CompositeType
+    //{
+    //    bool boolValue = true;
+    //    string stringValue = "Hello ";
+
+    //    [DataMember]
+    //    public bool BoolValue
+    //    {
+    //        get { return boolValue; }
+    //        set { boolValue = value; }
+    //    }
+
+    //    [DataMember]
+    //    public string StringValue
+    //    {
+    //        get { return stringValue; }
+    //        set { stringValue = value; }
+    //    }
+    //}
+}
---
+++ ProveedorServicio.svc.cs
@@ -0,0 +1,120 @@
+﻿using ProveedorServicio.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ProveedorServicio
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class ProveedorServicio : IProveedorServicio
+    {
+        //public string GetData(int value)
+        //{
+        //    return string.Format("You entered: {0}", value);
+        //}
+
+        //public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //{
+        //    if (composite == null)
+        //    {
+        //        throw new ArgumentNullException("composite");
+        //    }
+        //    if (composite.BoolValue)
+        //    {
+        //        composite.StringValue += "Suffix";
+        //    }
+        //    return composite;
+        //}
+        public bool InsertarProveedor(Modelo.Proveedor _proveedor)
+        {
+            try
+            {
+                using (var _context = new ProveedoresEntities())
+                {
+                    _context.Proveedors.Add(_proveedor);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // TODO Hacer algun tipo de LOG
+                return false;
+            }
+        }
+
+        public bool ActualizarProveedor(Modelo.Proveedor _proveedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EliminarProveedor(Modelo.Proveedor _proveedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Modelo.Proveedor> GetProveedores()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Modelo.Proveedor GetProveedor(int _idProveedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertarCategoria(Modelo.Categoria _categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ActualizarCategoria(Modelo.Categoria _categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EliminarCategoria(Modelo.Categoria _categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Modelo.Categoria> GetCategorias()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Modelo.Categoria GetCategoria(int _idCategoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertarProducto(Modelo.Producto _producto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ActualizarProducto(Modelo.Producto _producto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EliminarProducto(Modelo.Producto _producto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Modelo.Producto> GetProductos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Modelo.Producto GetProducto(int _idProducto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
